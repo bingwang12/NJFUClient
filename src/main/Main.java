f9e133f9b7d9a0a4c96b2cb91a8a4b5ecde9f566
bingwang12 <1935962822@qq.com>
2017-10-09 22:34:44 +0800

    view change

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,7 +2,7 @@
  * 
  */
 package main;
-
+import view.view;
 /**
  * 主类
  * @author WangZhiheng
@@ -14,7 +14,8 @@ public class Main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.print("Hello,World!");
+		view view1=new view();
+		view1.setVisible(true);
 	}
 
 }
