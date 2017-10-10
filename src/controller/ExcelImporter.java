1bdc5d685bcdc66ce002c2b2368a4a898054dc60
bingwang12 <1935962822@qq.com>
2017-10-10 18:40:25 +0800

    Revert "aaa"
    This reverts commit a29ecc61d73cda7830807d585427897a35ef54b5.

--- src/controller/ExcelImporter.java
+++ src/controller/ExcelImporter.java
@@ -13,7 +13,6 @@ import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import model.Student;
-import view.MainFrame;
 
 /**
  * 表格读取器
@@ -79,8 +78,5 @@ public class ExcelImporter {
 		}
 		return studentList;
 	}
-	public static void main(String[] args) {
-		ExcelImporter view1=new ExcelImporter();
-		view1.importExcel("d:\\aaa.xlsx");
-	}
+
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -13,6 +13,9 @@ public class Main {
 	/**
 	 * @param args
 	 */
-
+	public static void main(String[] args) {
+		MainFrame view1=new MainFrame();
+		view1.setVisible(true);
+	}
 
 }
