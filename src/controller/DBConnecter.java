b7bd57f3ae3a81305fa115862e74099abf20660a
wzh190015 <734649192@qq.com>
2017-10-10 17:22:34 +0800

    fix missing jar

---
+++ src/controller/DBConnecter.java
@@ -0,0 +1,51 @@
+/**
+ * 
+ */
+package controller;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * 数据库连接器
+ * 
+ * @author WangZhiheng
+ *
+ */
+public class DBConnecter {
+	/**
+	 * 数据库连接
+	 */
+	private Connection connection;
+
+	/**
+	 * 
+	 */
+	public DBConnecter() {
+		try {
+			Class.forName("org.sqlite.JDBC");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * 获取数据库连接
+	 * 
+	 * @param dbname
+	 *            数据库名
+	 * @return 数据库连接
+	 */
+	public Connection getConnection(String dbname) {
+		try {
+			connection = DriverManager.getConnection("jdbc:sqlite:" + dbname + ".db");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return connection;
+	}
+
+}
---
+++ src/controller/DBController.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package controller;
+
+/**
+ * 数据库控制器
+ * @author WangZhiheng
+ *
+ */
+public class DBController {
+
+	/**
+	 * 
+	 */
+	public DBController() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public static void createTable()
+	{
+		
+	}
+
+}
