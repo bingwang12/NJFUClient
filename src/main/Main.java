b1d14e8917ac5ff9ceaab692c1426ae8c2db1a5f
bingwang12 <1935962822@qq.com>
2017-10-10 12:56:40 +0800

    Revert "10101255"
    This reverts commit 08b324be6570d7939fd17f760c851838ad889104.

---
+++ src/main/Main.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package main;
+import view.view;
+/**
+ * 主类
+ * @author WangZhiheng
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		view view1=new view();
+		view1.setVisible(true);
+	}
+
+}
